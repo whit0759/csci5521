6fbf890f06afc4e7e021aee3d66efe8faedfbd8d
Christopher White <ckwhite@ieee.org>
2019-02-06 17:43:39 -0600

    Final Results

--- hwk0/MyPerceptron.m
+++ hwk0/MyPerceptron.m
@@ -18,17 +18,53 @@ err = 1;
 
 N = length(y);
 
-while err>0
-    for ii=1:N,
+figure;
+map = [0 0 0
+        1 0 0];
+colormap(map);
+scatter(X(:,1),X(:,2),[],y,'filled');
+xplt = xlim;
+yset = ylim;
+yplt = -w(1)*xplt/w(2);
+line(xplt,yplt);
+ylim(yset);
+title('Initial Scatter Plot and Weight');
+    
 
+
+while err>0
+    errcnt = 0;
+    for ii=1:N
+        
         wTXii = dot(w,X(ii,:));
 
-        if wTXii*y(ii) <=0,
-            w = w + y(ii)*X(ii,:);
+        if wTXii*y(ii) <=0
+            w = w + y(ii)*X(ii,:)';
         end
+        
+        if sign(wTXii)+sign(y(ii)) == 0
+            errcnt = errcnt+1;
+        end
+    end
+    
+    step = step + 1;
+    err = errcnt;
+    
+    if step>50
+        break
     end
 
 end
+
+figure;
+colormap(map);
+scatter(X(:,1),X(:,2),[],y,'filled');
+xplt = xlim;
+yset = ylim;
+yplt = -w(1)*xplt/w(2);
+line(xplt,yplt);
+ylim(yset);
+title('Final Scatter Plot and Weight');
 
 end
 
